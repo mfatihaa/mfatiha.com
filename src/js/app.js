96a0929217108250d07dd26ac72f03719c01c17b
mfatihaa <fatihjr1011@gmail.com>
2023-05-02 07:52:48 +0700

    First : Update 02/05/2023

---
+++ src/js/app.js
@@ -0,0 +1,59 @@
+// Disabled Right Click
+document.addEventListener("contextmenu", (event) => event.preventDefault());
+
+// Disabled Inspect
+document.addEventListener("keydown", function (event) {
+  if (event.keyCode == 123) {
+    event.preventDefault();
+  } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
+    event.preventDefault();
+  } else if (event.ctrlKey && event.shiftKey && event.keyCode == 74) {
+    event.preventDefault();
+  } else if (event.ctrlKey && event.keyCode == 85) {
+    event.preventDefault();
+  }
+});
+
+// Realtime
+function times() {
+  var realtime = new Date().toLocaleTimeString();
+  var clock = document.getElementById("clock");
+
+  clock.innerHTML = "Sekarang Pukul" + " " + realtime + " " + "WIB";
+  setTimeout(times, 1000);
+}
+
+// View Visitors Page
+var countVisitor = localStorage.getItem("countVisitor");
+if (countVisitor === null) {
+  countVisitor = 0;
+} else {
+  countVisitor = parseInt(countVisitor);
+}
+countVisitor++;
+localStorage.setItem("countVisitor", countVisitor);
+document.getElementById("visitor").innerHTML =
+  "Jumlah Pengunjung Halaman : " + countVisitor + " " + "Pengunjung";
+
+// Change Input Card
+var card = document.getElementById("bg-custom");
+var input = document.getElementById("input-change");
+
+input.addEventListener("input", () => {
+  card.style.borderRadius = input.value;
+  card.style.background = input.value;
+});
+
+// Multipe Title
+var titleText = [
+  "Welcome To MyLink",
+  "Muhammad Fatih Aulia",
+  "I'am Fresh Graduate",
+];
+
+let counter = 0;
+
+setInterval(function () {
+  document.title = titleText[counter % titleText.length];
+  counter++;
+}, 2000);
